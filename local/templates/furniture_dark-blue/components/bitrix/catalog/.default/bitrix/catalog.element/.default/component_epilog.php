dec7678f1612d316a9ca557bb51458e7bcf40707
Kate <Katrinekatek@gmail.com>
2019-11-06 15:42:23 +0300

    ех2-109

---
+++ local/templates/furniture_dark-blue/components/bitrix/catalog/.default/bitrix/catalog.element/.default/component_epilog.php
@@ -0,0 +1,4 @@
+<?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
+
+
+$APPLICATION->SetPageProperty('STYLE_PAGE', "background-image: url(" . $arResult["DETAIL_PICTURE"]["SRC"] . "); background-size: contain;");
--- local/templates/furniture_dark-blue/header.php
+++ local/templates/furniture_dark-blue/header.php
@@ -32,7 +32,7 @@ IncludeTemplateLangFile(__FILE__);
 <body>
 	<div id="page-wrapper">
 	<div id="panel"><?$APPLICATION->ShowPanel();?></div>
-		<div id="header">
+		<div id="header" style="<?$APPLICATION->ShowProperty('STYLE_PAGE');?>">
 			
 			<table id="logo">
 				<tr>
